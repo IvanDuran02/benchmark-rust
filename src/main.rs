ed8e02012c8c39b9b5c5a14e8471281c3bb76a96
Ivan Duran <iamivanduran02@gmail.com>
2024-03-13 03:09:30 -0400

    added a test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use axum::{routing::get, Router};
+use axum::{response::IntoResponse, routing::get, Router};
+use axum_test::TestServer;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, world" }));
+    let app = create_app();
 
     match tokio::net::TcpListener::bind("0.0.0.0:3000").await {
         Ok(listener) => {
@@ -13,4 +14,29 @@ async fn main() {
         }
         Err(err) => eprintln!("Failed to bind to address: {}", err),
     }
+}
+
+async fn hello() -> impl IntoResponse {
+    "Hello, world"
+}
+
+async fn foo() -> impl IntoResponse {
+    "bar"
+}
+
+#[tokio::test]
+async fn test_foo_endpoint() {
+    if let Ok(app) = create_app().try_into() {
+        let client = TestServer::new::<Router>(app).unwrap();
+
+        let response = client.get("/foo").await;
+        assert_eq!(response.status_code(), 200);
+        assert_eq!(response.text(), "bar");
+    } else {
+        panic!("Failed to create Axum application for testing")
+    }
+}
+
+fn create_app() -> Router {
+    Router::new().route("/", get(hello)).route("/foo", get(foo))
 }
